9fe5b0c7d580db68e48d7fadcaece4f988a42661
joshuarossi <maximojoshuarossi@gmail.com>
2024-07-09 05:15:00 -0600

    get rid of title on dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -18,7 +18,6 @@ const Dashboard = () => {
 
   return (
     <div className='p-4'>
-      <h1 className='text-2xl font-bold mb-4'>LUT Dashboard</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {luts.map(lut => (
           <LutCard key={lut.id} lut={lut} />
